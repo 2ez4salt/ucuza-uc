541b6d797d8cbdce519c8d1e0b20eae6e57c02c5
İsa Kaan Erdoğan <isakaanerdogan@gmail.com>
2019-01-01 04:15:32 +0300

    index, readme, about(olabilir) düzenlemeleri

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# ucuza-uc
+# Ucuza Uc
+
+[Ucuza Uc](https://github.com/2ez4salt/ucuza-uc/#) is open-source mobile app that development by 3 student. This app runs on [Ionic](https://ionicframework.com/).
+> Using [Ionic 3](https://github.com/ionic-team/ionic-v3) on it.
+
+<table>
+  <thead>
+    <tr>
+      |<th>Side</th>| 
+      <th>Developers</th>|
+    <tr>
+  </thead>
+  <tbody>
+    <tr>
+      |<td>UI design  </td>|
+      <td><a href="https://https://github.com/kaanerdogan">İ. Kaan Erdoğan</a></td>|
+    </tr>
+    <tr>
+      |<td>Back-end </td>|
+      <td><a href="https://github.com/2ez4salt">M.Talha Salt</a></td>|
+    </tr>
+    <tr>
+      |<td>Others</td>|
+      <td> A.Burak Demir </td>|
+    </tr>
+  </tbody>
+</table>
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,5 +10,23 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  /*
+    airports: Array<any> = [
+    {code: "IST", airportName: "İstanbul Atatürk Havalimanı", province: "İstanbul", country: "Türkiye"},
+    {code: "SAW", airportName: "Sabiha Gökçen Havalimanı", province: "İstanbul", country: "Türkiye"},
+    {code: "ESB", airportName: "Esenboğa Havalimanı", province: "Ankara", country: "Türkiye"},
+    {code: "ADB", airportName: "Adnan Menderes Havalimanı", province: "İzmir", country: "Türkiye"},
+    {code: "ADA", airportName: "Şakirpaşa Havalimanı", province: "Adana", country: "Türkiye"},
+    {code: "AYT", airportName: "Antalya Havalimanı", province: "Antalya", country: "Türkiye"},
+    {code: "BRE", airportName: "Bremen Havalimanı", province: "Bremen", country: "Almanya"},
+    {code: "DTM", airportName: "Dortmund Havalimanı", province: "Dortmund", country: "Almanya"},
+    {code: "DUS", airportName: "Duesseldorf Havalimanı", province: "Dusseldorf", country: "Almanya"},
+    {code: "JFK", airportName: "John F Kennedy Havalimanı", province: "New York", country: "ABD"},
+    {code: "WSG", airportName: "County Havalimanı", province: "Washington", country: "ABD"},
+    {code: "LAX", airportName: "Los Angeles Havalimanı", province: "Antalya", country: "ABD"},
+    {code: "SKG", airportName: "Makedonia Havalimanı", province: "Selanik", country: "Yunanistan"},
+    {code: "MED", airportName: "Madinah Havalimanı", province: "Medine", country: "Suudi Arabistan"},
+  ]
+  */
 
 }
